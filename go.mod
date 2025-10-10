8f7519382930db794ffc7b2c11b5d3f0121e6e08
I-Missha <i.missha.d@gmail.com>
2025-10-10 15:40:48 +0700

    done with batcher + buffer, todo: cqe worker

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.9
 require github.com/godzie44/go-uring v0.0.0-20250501163612-d16a9e597639
 
 require (
+	github.com/jonoton/go-ringbuffer v1.6.0 // indirect
 	github.com/libp2p/go-sockaddr v0.1.1 // indirect
 	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
 )
--- net/dial.go
+++ net/dial.go
@@ -2,40 +2,12 @@ package net
 
 import (
 	"context"
-	"syscall"
 	gonet "net"
 
 	"github.com/godzie44/go-uring/uring"
 )
 
-type Dialer struct {
-	ring uring.Ring
-
-}
-
 func (d *Dialer) DialContext(ctx context.Context, network, address string) (gonet.Conn, error) {
-	sockFd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_CLOEXEC, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	/*
-	if err = setDefaultListenerSockopts(sockFd); err != nil {
-		return nil, err
-	}
-	*/
-
-	addr, err := gonet.ResolveTCPAddr(network, address)
-	if err != nil {
-		return nil, err
-	}
-	if err != nil {
-		return nil, err
-	}
-	connOp := uring.Connect(uintptr(sockFd), addr)
-	sqe := uring.SQEntry{}
-	connOp.PrepSQE(&sqe)
-	d.ring.QueueSQE(connOp, 0, 0)
 
 	return sqe.Fd(), err
 }
