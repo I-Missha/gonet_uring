a0d322c898c8408324bae178a2497510339594f2
I-Missha <i.missha.d@gmail.com>
2025-11-12 20:42:57 +0700

    intermediate commit

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -5,6 +5,8 @@ import (
 	"net"
 	"os"
 	"runtime"
+	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -23,7 +25,7 @@ func TestMain(m *testing.M) {
 
 func BenchmarkClientGetEndToEnd4000TCP(b *testing.B) {
 	b.Run("io_uring", func(b *testing.B) {
-		benchmarkClientGetEndToEndTCPNoKeepAlive(b, 2000, func(addr string) (net.Conn, error) {
+		benchmarkClientGetEndToEndTCPNoKeepAlive(b, 125, func(addr string) (net.Conn, error) {
 			dialer := mynet.NewUringDialer()
 			return dialer.DialContext(context.TODO(), "tcp", addr)
 		})
@@ -32,30 +34,53 @@ func BenchmarkClientGetEndToEnd4000TCP(b *testing.B) {
 	time.Sleep(time.Second * 20)
 
 	b.Run("net", func(b *testing.B) {
-		benchmarkClientGetEndToEndTCPNoKeepAlive(b, 2000, func(addr string) (net.Conn, error) {
+		benchmarkClientGetEndToEndTCPNoKeepAlive(b, 125, func(addr string) (net.Conn, error) {
 			return net.Dial("tcp6", addr)
 		})
 	})
 }
 
 func benchmarkClientGetEndToEndTCPNoKeepAlive(b *testing.B, parallelism int, dial fasthttp.DialFunc) {
-	addr := "[2a02:6b8:c02:901:0:fc7e:0:36c]:8543" // TODO: set addr to config.yaml
+	addresses := make([]string, 0, 5)
+
+	addresses = append(addresses,
+		"2a02:6b8:c02:901:0:fc7e:0:36c",
+		"2a02:6b8:c02:901:0:fc7e:0:3cf",
+		"2a02:6b8:c02:901:0:fc7e:0:398",
+		"2a02:6b8:c02:901:0:fc7e:0:3df",
+		"2a02:6b8:c02:901:0:fc7e:0:345",
+		"2a02:6b8:c02:901:0:fc7e:0:261",
+		"2a02:6b8:c02:901:0:fc7e:0:1a8")
 
 	// Клиенту теперь не нужен большой пул соединений, но оставим его,
 	c := &fasthttp.Client{
 		// MaxConnsPerHost больше не играет роли для переиспользования,
 		// но все еще ограничивает количество одновременных запросов в полете.
-		MaxConnsPerHost:    runtime.GOMAXPROCS(-1) * parallelism * 8,
+		MaxConnsPerHost:    runtime.GOMAXPROCS(-1) * parallelism,
 		Dial:               dial,
-		MaxConnWaitTimeout: time.Second * 2,
+		MaxConnWaitTimeout: time.Second * 5,
 	}
 
 	requestURI := "/hello"
-	url := "http://" + addr + requestURI
+
+	amountPorts := 10
+	basePort := 8543
+	urls := make([]string, amountPorts*len(addresses))
+	for i := 0; i < amountPorts; i++ {
+		port := basePort + i
+		for j := 0; j < len(addresses); j++ {
+			addr := addresses[j]
+			resAddr := net.JoinHostPort(addr, strconv.Itoa(port))
+
+			urls[i*len(addresses)+j] = "http://" + resAddr + requestURI
+		}
+	}
 	b.SetParallelism(parallelism * runtime.GOMAXPROCS(-1))
 
 	b.ResetTimer()
 
+	amount := uint32(len(urls))
+	var counter uint32
 	b.RunParallel(func(pb *testing.PB) {
 		req := fasthttp.AcquireRequest()
 		defer fasthttp.ReleaseRequest(req) // Убедимся, что объект вернется в пул при выходе из горутины
@@ -63,12 +88,13 @@ func benchmarkClientGetEndToEndTCPNoKeepAlive(b *testing.B, parallelism int, dia
 		resp := fasthttp.AcquireResponse()
 		defer fasthttp.ReleaseResponse(resp) // То же самое для ответа
 
-		req.SetRequestURI(url)
+		// req.SetRequestURI(urls[int(counter%uint32(amount))])
 
 		req.Header.Set("Connection", "close")
 
 		for pb.Next() {
 			// Выполняем запрос с помощью c.Do
+			req.SetRequestURI(urls[int(atomic.AddUint32(&counter, 1)%uint32(amount))])
 			if err := c.Do(req, resp); err != nil {
 				b.Fatalf("unexpected error: %v", err)
 			}
---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net" // Импортируем стандартный пакет net
+	"os"
+	"time"
+	"sync"
+
+	"github.com/valyala/fasthttp"
+)
+
+var withSleep = false
+
+func echoHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Printf("Received request from %s\n", ctx.RemoteAddr())
+	ctx.SetContentType("text/plain; charset=utf-8")
+	ctx.Write(ctx.RequestURI())
+	if withSleep {
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func main() {
+	args := os.Args
+	if len(args) > 1 {
+		withSleep = true
+	}
+
+	var wg sync.WaitGroup
+	basePort := 8543
+	for i := 0; i < 10; i++ {
+		addr := fmt.Sprintf("[::]:%d", basePort+i)
+		ln, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.Fatalf("Failed to create listener: %s", err)
+		}
+
+		fmt.Printf("Starting dual-stack echo server on %s\n", ln.Addr().String())
+
+		wg.Add(1)
+		go func() {
+			if err := fasthttp.Serve(ln, echoHandler); err != nil {
+				log.Fatalf("Error in fasthttp.Serve: %s", err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
